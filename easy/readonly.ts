14f9a5987cf3f5b3d07a103f9ff81dad142cb1f0
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-12-04 00:47:42 +1300

    add(easy): readonly

---
+++ easy/readonly.ts
@@ -0,0 +1,16 @@
+interface Todo {
+  title: string;
+  description: string;
+}
+
+const todo: MyReadonly<Todo> = {
+  title: "Hey",
+  description: "foobar",
+};
+
+todo.title = "Hello"; // Error: cannot reassign a readonly property
+todo.description = "barFoo"; // Error: cannot reassign a readonly property
+
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P];
+};
