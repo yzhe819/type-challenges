b7d32c0abeef87a119a94c4745b5352d07f2abb2
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-12-05 14:00:25 +1300

    add(medium): deep readonly

---
+++ medium/deep-readonly.ts
@@ -0,0 +1,25 @@
+import { Equal, Expect } from "@type-challenges/utils";
+
+type X = {
+  x: {
+    a: 1;
+    b: "hi";
+  };
+  y: "hey";
+};
+
+type Expected = {
+  readonly x: {
+    readonly a: 1;
+    readonly b: "hi";
+  };
+  readonly y: "hey";
+};
+
+type Todo = DeepReadonly<X>; // should be same as `Expected`
+
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends never ? T[K] : DeepReadonly<T[K]>;
+};
+
+type test = Expect<Equal<Todo, Expected>>;
