c90ef661a82b26a9cbbd20eb06832d917dc00623
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-12-07 03:23:04 +1300

    add(medium): lookup

---
+++ medium/lookup.ts
@@ -0,0 +1,20 @@
+interface Cat {
+  type: "cat";
+  breeds: "Abyssinian" | "Shorthair" | "Curl" | "Bengal";
+}
+
+interface Dog {
+  type: "dog";
+  breeds: "Hound" | "Brittany" | "Bulldog" | "Boxer";
+  color: "brown" | "white" | "black";
+}
+
+type MyDog = LookUp<Cat | Dog, "dog">; // expected to be `Dog`
+
+type LookUp<T, K> = T extends {
+  type: infer U;
+}
+  ? U extends K
+    ? T
+    : never
+  : never;
