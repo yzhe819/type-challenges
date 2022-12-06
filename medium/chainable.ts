16b85167d2e4e0186d2c2326803ab847b803a56d
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-12-07 02:57:47 +1300

    add(medium): chainable

---
+++ medium/chainable.ts
@@ -0,0 +1,24 @@
+declare const config: Chainable;
+
+const result = config
+  .option("foo", 123)
+  .option("name", "type-challenges")
+  .option("bar", { value: "Hello World" })
+  .get();
+
+// 期望 result 的类型是：
+interface Result {
+  foo: number;
+  name: string;
+  bar: {
+    value: string;
+  };
+}
+
+type Chainable<T = {}> = {
+  option: <K extends string, V>(
+    key: K,
+    value: V
+  ) => Chainable<T & { [P in K]: V }>;
+  get: () => T;
+};
